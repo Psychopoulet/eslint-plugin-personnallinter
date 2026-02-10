2903490c10eb9df6e14f98509eb8ab06f6a8bc36
Sébastien VIDAL <meona@live.fr>
2026-02-10 15:58:48 +0100

    eslint

--- test/0_compilation_typescript.js
+++ test/0_compilation_typescript.js
@@ -1,70 +1,67 @@
-
-"use strict";
-
 // deps
 
-	// natives
-	const { exec } = require("child_process");
-	const { join } = require("path");
-	const { unlink } = require("fs");
+    // natives
+    const { exec } = require("child_process");
+    const { join } = require("path");
+    const { unlink } = require("fs");
 
 // consts
 
-	const MAX_TIMEOUT = 10000;
+    const MAX_TIMEOUT = 10000;
 
 // tests
 
 describe("compilation typescript", () => {
 
-	describe("commonjs", () => {
+    describe("commonjs", () => {
 
-		after((done) => {
+        after((done) => {
 
-			unlink(join(__dirname, "typescript", "compilation.cjs"), (err) => {
-				return err ? done(err) : done();
-			});
+            unlink(join(__dirname, "typescript", "compilation.cjs"), (err) => {
+                return err ? done(err) : done();
+            });
 
-		});
+        });
 
-		it("should compile typescript file", () => {
+        it("should compile typescript file", () => {
 
-			return new Promise((resolve, reject) => {
+            return new Promise((resolve, reject) => {
 
-				const args = [
-					"npx tsc",
-					join(__dirname, "typescript", "compilation.cts"),
-					"--target es6",
-					"--module commonjs",
-					"--esModuleInterop"
-				];
+                const args = [
+                    "npx tsc",
+                    join(__dirname, "typescript", "compilation.cts"),
+                    "--target es6",
+                    "--module commonjs",
+                    "--esModuleInterop"
+                ];
 
-				exec(args.join(" "), {
-					"cwd": join(__dirname, ".."),
-					"windowsHide": true
-				}, (err) => {
-					return err ? reject(err) : resolve();
-				});
+                exec(args.join(" "), {
+                    "cwd": join(__dirname, ".."),
+                    "windowsHide": true
+                }, (err) => {
+                    return err ? reject(err) : resolve();
+                });
 
-			});
+            });
 
-		}).timeout(MAX_TIMEOUT);
+        }).timeout(MAX_TIMEOUT);
 
-		it("should exec compiled typescript file", (done) => {
+        it("should exec compiled typescript file", (done) => {
 
-			const args = [
-				"node",
-				join(__dirname, "typescript", "compilation.cjs")
-			];
+            const args = [
+                "node",
+                join(__dirname, "typescript", "compilation.cjs")
+            ];
 
-			exec(args.join(" "), {
-				"cwd": join(__dirname, ".."),
-				"windowsHide": true
-			}, (err) => {
-				return err ? done(err) : done();
-			});
+            exec(args.join(" "), {
+                "cwd": join(__dirname, ".."),
+                "windowsHide": true
+            }, (err) => {
+                return err ? done(err) : done();
+            });
 
-		}).timeout(MAX_TIMEOUT);
+        }).timeout(MAX_TIMEOUT);
 
-	});
+    });
 
 });
